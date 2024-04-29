b7037212ca7d2b25685d8df16a56ec24506b418a
Warren <rabbitsinwarrens@gmail.com>
2024-04-30 04:31:19 +1000

    feat(task2): submit to checker

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""
+Provides measure_time function.
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+async def measure_time(n: int, max_delay: int) -> float:
+    """
+    Return the time to wait for n calls to wait_random.
+    """
+    start = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end = time.time()
+    total_time = end - start
+    return total_time
