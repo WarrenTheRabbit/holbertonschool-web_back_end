8769f41788b55b8e98f9b7e517ec26997f2a328a
Warren <rabbitsinwarrens@gmail.com>
2024-04-30 04:07:59 +1000

    fix(task1): reference function instead of module

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@ Demonstrates async task scheduling and execution.
 """
 import asyncio
 from typing import List
-wait_random = __import__("0-basic_async_syntax")
+wait_random = __import__("0-basic_async_syntax").wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
