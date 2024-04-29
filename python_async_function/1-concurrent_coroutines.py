83a16c5e31473815e60ce0a7ad4a31e58aa52be6
Warren <rabbitsinwarrens@gmail.com>
2024-04-30 04:02:01 +1000

    feat(task1): gather multiple co-routines

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Demonstrates async task scheduling and execution.
+"""
+import asyncio
+from typing import List
+wait_random = __import__("python_async_function/0-basic_async_syntax.py")
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """
+    Returns ascending list of delays as floats.
+    """
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    results = asyncio.gather(*tasks)
+    return sorted(results)
