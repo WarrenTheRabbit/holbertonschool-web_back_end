651abfe835df05fbbc3850cbf3650f6132825b7a
Warren <rabbitsinwarrens@gmail.com>
2024-04-30 04:52:44 +1000

    fix(task0): use Generator for ReturnType

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -4,10 +4,10 @@ Documents module.
 """
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Returns 10 random numbers between 0 and 10 asynchronously.
     """
