e8f40a01ca5c073c1760d3dbabc05cca8859be82
Warren <rabbitsinwarrens@gmail.com>
2024-04-30 04:32:56 +1000

    fix(task2): return average wait time

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -15,4 +15,5 @@ def measure_time(n: int, max_delay: int) -> float:
     asyncio.run(wait_n(n, max_delay))
     end = time.time()
     total_time = end - start
-    return total_time
+    average_time = total_time / n
+    return average_time
