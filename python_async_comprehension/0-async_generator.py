9bd7e5b9e747306c9acbc7bc2b2f68a5a6006db9
Warren <rabbitsinwarrens@gmail.com>
2024-04-30 04:51:27 +1000

    fix(task0): pass 2 parameters to AsyncGenerator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,9 +7,9 @@ import random
 from typing import AsyncGenerator
 
 
-async def async_generator() -> AsyncGenerator[float, None, None]:
+async def async_generator() -> AsyncGenerator[float, None]:
     """
-    Returns 10 random numbers between 0 and 10.
+    Returns 10 random numbers between 0 and 10 asynchronously.
     """
     for _ in range(10):
         await asyncio.sleep(1)
