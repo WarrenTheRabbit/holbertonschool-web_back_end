2999adc4db49013e4dd300a43cf9524d2c14955e
Warren <rabbitsinwarrens@gmail.com>
2024-05-11 14:22:49 +1000

    refactor(task1): remove map and return manually initialised classrooms

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,7 +1,5 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  // return [ new ClassRoom(19), new ClassRoom(20), new ClassRoom(34) ]
-  const roomSizes = [19, 20, 34];
-  return roomSizes.map((size) => new ClassRoom(size));
+  return [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
 }
