790e5d2562695e3985eee195ce73d7064ccaa748
Warren <rabbitsinwarrens@gmail.com>
2024-05-11 13:01:06 +1000

    refactor(task1): map constructor over room sizes

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -1,5 +1,7 @@
-import ClassRoom from "./0-classroom";
+import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  return [ new ClassRoom(19), new ClassRoom(20), new ClassRoom(34) ]
+  // return [ new ClassRoom(19), new ClassRoom(20), new ClassRoom(34) ]
+  const roomSizes = [19, 20, 35]
+  return roomSizes.map(size => new ClassRoom(size));
 }
