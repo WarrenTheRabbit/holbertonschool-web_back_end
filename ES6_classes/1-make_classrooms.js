0b41aa7d5cada87916681aafda60a465789a642a
Warren <rabbitsinwarrens@gmail.com>
2024-05-11 13:06:10 +1000

    fix(task1): correct roomSize value

--- ES6_classes/1-make_classrooms.js
+++ ES6_classes/1-make_classrooms.js
@@ -2,6 +2,6 @@ import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
   // return [ new ClassRoom(19), new ClassRoom(20), new ClassRoom(34) ]
-  const roomSizes = [19, 20, 35]
-  return roomSizes.map(size => new ClassRoom(size));
+  const roomSizes = [19, 20, 34];
+  return roomSizes.map((size) => new ClassRoom(size));
 }
