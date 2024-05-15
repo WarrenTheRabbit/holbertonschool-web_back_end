6add2191f0357be94b1e0487e61f6147b719c120
Warren <rabbitsinwarrens@gmail.com>
2024-05-15 14:59:51 +1000

    feat(task2): create HATEOAS format response

---
+++ pagination/2-hypermedia_pagination.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""
+Simple helper function
+"""
+
+
+import csv
+import math
+from typing import List
+
+
+def index_range(page, page_size):
+    """
+    Returns the start and end indices for a given page and page size.
+
+    Args:
+        page (int): The page number.
+        page_size (int): The number of items per page.
+
+    Returns:
+        tuple: A tuple containing the start and end indices.
+
+    """
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return dataset for the given page and page size."""
+        assert isinstance(page, int) and page >= 1
+        assert isinstance(page_size, int) and page_size >= 1
+
+        start_index, end_index = index_range(page, page_size)
+        dataset = self.dataset()
+        dataset_max_range = len(dataset)
+        if start_index >= dataset_max_range:
+            return []
+        return dataset[start_index: end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """
+        Create an obj including pagination info and return it.
+        """
+        data = self.get_page(page, page_size)
+        total_items = len(self.dataset())
+        total_pages = math.ceil(total_items / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            'page_size': page_size if len(data) else 0,
+            'page': page,
+            'data': data,
+            'next_page': next_page if page < total_pages else None,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
